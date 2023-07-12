c5d16421daae796a3e4862ab3071ef4c2d95eb33
Angel <angel-ortega27@hotmail.com>
2023-07-12 13:57:04 -0700

    added roll dice button to randomize dice

--- src/App.js
+++ src/App.js
@@ -3,20 +3,25 @@ import React from "react";
 import Die from "./components/Die";
 
 export default function App() {
+  const [dice, setDice] = React.useState(allNewDice);
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.floor(Math.random() * (6 - 1 + 1) + 1));
+    }
+    return newDice;
+  }
+  function rollDice() {
+    setDice(allNewDice());
+  }
+  const diceElements = dice.map((die) => <Die value={die} />);
   return (
     <main>
-      <div className="die-container">
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-      </div>
+      <div className="die-container">{diceElements}</div>
+      <button className="roll-button" onClick={rollDice}>
+        Roll
+      </button>
     </main>
   );
 }
